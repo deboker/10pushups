18e56fe205bf3ac13ecb4cee2144985cb7252a12
deboker <117391017+deboker@users.noreply.github.com>
2024-03-30 22:18:27 +0100

    responsive design

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,20 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./index.html",
+    "./src/**/*.{html,js}", // Adjust this path based on where your files are located
+  ],
   theme: {
-    extend: {},
+    extend: {
+      // Custom colors
+      colors: {
+        'custom-brown': '#291b1e', // Example custom color
+        'custom-peach': '#e19978', // Another custom color
+      },
+      fontFamily: {
+        'sans': ['ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe UI', 'Roboto', 'Helvetica Neue', 'Arial', 'Noto Sans', 'sans-serif', 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol', 'Noto Color Emoji'], // Example of extending the default sans-serif font family
+      },
+      // Add other theme customizations here
+    },
   },
   plugins: [],
 }
-
